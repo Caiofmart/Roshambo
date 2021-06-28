21a8f6bf4abe8f1e813d87d013c19c8fd6a2d068
Caio <caiofmart@gmail.com>
2021-06-28 17:29:55 -0300

    Initial Commit

---
+++ Roshambo/RoshamboApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoshamboApp.swift
+//  Roshambo
+//
+//  Created by Caio Feitoza Martins on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct RoshamboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
